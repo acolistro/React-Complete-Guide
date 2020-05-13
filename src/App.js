733ba380e263b5f3348b4bef6d2e408615c0dd44
acolistro <arcolistro@gmail.com>
2020-05-12 17:35:25 -0700

    revert back to object oriented using class, add click function to p element

--- src/App.js
+++ src/App.js
@@ -1,50 +1,46 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-const app = props => {
-  const [ personsState, setPersonsState ] = useState({
-      persons: [
-        { name: 'Max', age: 28 },
-        { name: 'Manu', age: 29 },
-        { name: 'Lester', age: 26 }
+class App extends Component {
+  state = {
+    persons: [
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 29 },
+      { name: 'Lester', age: 26 }
     ]
-  });
+  } 
 
-  const handleSwitchName = () => {
+  handleSwitchName = () => {
     // console.log('was clicked!');
-    setPersonsState({    
+    this.setState({    
       persons: [
         { name: 'Maximillian', age: 28 },
         { name: 'Manu', age: 29 },
         { name: 'Lester', age: 45 }
       ] 
     });
-  };
+  }
 
-
+  render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <button onClick={handleSwitchName}>Switch Name</button>
-        <Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
-        <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>My Hobbies: Racing</Person>
-        <Person name={personsState.persons[2].name} age={personsState.persons[2].age}/>
+        <button onClick={this.handleSwitchName}>Switch Name</button>
+        <Person 
+        name={this.state.persons[0].name} 
+        age={this.state.persons[0].age}/>
+        <Person 
+        name={this.state.persons[1].name} 
+        age={this.state.persons[1].age}
+        click={this.handleSwitchName}>My Hobbies: Racing</Person>
+        <Person 
+        name={this.state.persons[2].name} 
+        age={this.state.persons[2].age}/>
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'I\'m a React App!!!'));
   }
+}
 
-export default app;
-
-
-//Object oriented approach code
-// state = {
-//   persons: [
-//     { name: 'Max', age: 28 },
-//     { name: 'Manu', age: 29 },
-//     { name: 'Lester', age: 26 }
-//   ]
-// } 
-
-
+export default App;
