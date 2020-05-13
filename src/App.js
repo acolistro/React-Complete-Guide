acf02420c7a75ac7d025a8aee80bd1bea8717e5a
acolistro <arcolistro@gmail.com>
2020-05-12 17:07:24 -0700

    update app class to be a functional component

--- src/App.js
+++ src/App.js
@@ -1,39 +1,50 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-  state = {
-    persons: [
-      { name: 'Max', age: 28 },
-      { name: 'Manu', age: 29 },
-      { name: 'Lester', age: 26 }
+const app = props => {
+  const [ personsState, setPersonsState ] = useState({
+      persons: [
+        { name: 'Max', age: 28 },
+        { name: 'Manu', age: 29 },
+        { name: 'Lester', age: 26 }
     ]
-  } 
+  });
 
-  handleSwitchName = () => {
+  const handleSwitchName = () => {
     // console.log('was clicked!');
-    this.setState({    
+    setPersonsState({    
       persons: [
         { name: 'Maximillian', age: 28 },
         { name: 'Manu', age: 29 },
         { name: 'Lester', age: 45 }
       ] 
     });
-  }
+  };
 
-  render() {
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <button onClick={this.handleSwitchName}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        <button onClick={handleSwitchName}>Switch Name</button>
+        <Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
+        <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>My Hobbies: Racing</Person>
+        <Person name={personsState.persons[2].name} age={personsState.persons[2].age}/>
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'I\'m a React App!!!'));
   }
-}
 
-export default App;
+export default app;
+
+
+//Object oriented approach code
+// state = {
+//   persons: [
+//     { name: 'Max', age: 28 },
+//     { name: 'Manu', age: 29 },
+//     { name: 'Lester', age: 26 }
+//   ]
+// } 
+
+
