8c69f38934f480a6c17b52960aaa5243868c5344
acolistro <arcolistro@gmail.com>
2020-05-14 15:39:20 -0700

    add toddle handler and ternary expression in render method to toggle display

--- src/App.js
+++ src/App.js
@@ -41,23 +41,32 @@ class App extends Component {
       cursor: 'pointer'
     };
 
+    handleTogglePersons = () => {
+      showPersons: false
+    }
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <button 
         style={style}
-        onClick={() => this.handleSwitchName("Maximillian!!")}>Switch Name</button>
-        <Person 
-        name={this.state.persons[0].name} 
-        age={this.state.persons[0].age}/>
-        <Person 
-        name={this.state.persons[1].name} 
-        age={this.state.persons[1].age}
-        click={this.handleSwitchName.bind(this, 'Max!')}
-        changed={this, this.handleNameChanged}>My Hobbies: Racing</Person>
-        <Person 
-        name={this.state.persons[2].name} 
-        age={this.state.persons[2].age}/>
+        onClick={() => this.handleTogglePersons("Maximillian!!")}>Switch Name</button>
+        { 
+          this.state.showPersons ?
+            <div>
+              <Person 
+              name={this.state.persons[0].name} 
+              age={this.state.persons[0].age}/>
+              <Person 
+              name={this.state.persons[1].name} 
+              age={this.state.persons[1].age}
+              click={this.handleSwitchName.bind(this, 'Max!')}
+              changed={this, this.handleNameChanged}>My Hobbies: Racing</Person>
+              <Person 
+              name={this.state.persons[2].name} 
+              age={this.state.persons[2].age}/>
+            </div> : null
+        }
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'I\'m a React App!!!'));
